302634993ef90c9b7f9ff3d655b67b21c2e32997
Alida <alida_biro@yahoo.com>
2019-11-25 10:30:46 +0200

    Displaying the Orders in a Recycler View

--- app/src/main/java/com/example/sapidiner/Adapters/OrdersAdapter.java
+++ app/src/main/java/com/example/sapidiner/Adapters/OrdersAdapter.java
@@ -1,13 +1,75 @@
 package com.example.sapidiner.Adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class OrdersAdapter {
+import com.example.sapidiner.Classes.Orders;
+import com.example.sapidiner.R;
+
+import java.util.ArrayList;
+
+public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.MyViewHolder> {
 
 
+    private Context context;
+    private ArrayList<Orders> orders;
 
+    public OrdersAdapter(Context context, ArrayList<Orders> orders) {
+        this.context = context;
+        this.orders = orders;
+    }
+
+    @NonNull
+    @Override
+
+
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.view_orders,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+
+        holder.userId.setText(orders.get(position).getClientid());
+        String o = "";
+        String space = " ";
+        if(orders.get(position).getOrders() != null)
+        {
+            for(String ord : orders.get(position).getOrders())
+            {
+                
+                o = o + space + ord;
+            }
+
+        }
+
+        holder.order.setText(o);
+        holder.price.setText("" + orders.get(position).getPrice());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return orders.size();
+    }
+
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView userId,order, price;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userId = (TextView) itemView.findViewById(R.id.textViewId);
+            order = (TextView) itemView.findViewById(R.id.textViewOrders);
+            price = (TextView) itemView.findViewById(R.id.textViewPrice);
+        }
+    }
 }
